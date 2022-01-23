a030a102d3e3ce3759f5c25f99f448d7780aea94
Webber <webber@takken.io>
2022-01-23 17:41:46 +0100

    feat: gists now have better formatting

--- components/pages/Gists/GistPage.tsx
+++ components/pages/Gists/GistPage.tsx
@@ -15,6 +15,8 @@ const mapGitHubLanguageToSupportedLanguage = (rawLanguage): Language => {
 
   if (isLanguageSupported(language)) return language as Language
   if (language.startsWith('git')) return 'git'
+  if (language.startsWith('shell')) return 'bash'
+  if (language.startsWith('powershell')) return 'bash'
 
   return undefined
 }
@@ -39,6 +41,7 @@ const GistPage = ({ gist }: Props) => {
             </div>
 
             <CodeEditor code={file.content} language={mapGitHubLanguageToSupportedLanguage(language)} />
+            <div style={{ paddingBottom: '1em' }} />
           </>
         )
       })}
