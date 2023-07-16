7a18f266ccd0f0a98d9614bf4bf6b75695a11f53
Webber <webber@takken.io>
2023-07-16 18:28:04 +0200

    chore: upgrade packages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'webbertakken', // Usually your GitHub org/user name.
+  organizationName: 'webbertakken', // Usually your GitHub org/username.
   projectName: 'takken.io', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
