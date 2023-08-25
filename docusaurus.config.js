7d3fa5f80d0d96d89291a864a4325ccd82b22611
Webber Takken <webber@takken.io>
2023-08-26 00:55:22 +0200

    feat: add git notes (#108)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -222,7 +222,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['powershell', 'rust'],
+        additionalLanguages: ['powershell', 'rust', 'ini'],
       },
       algolia: {
         // The application ID provided by Algolia
