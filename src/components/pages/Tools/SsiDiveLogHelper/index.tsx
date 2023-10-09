0a343709d8755e5687b8933b2df78fb5515d84d8
Webber <webber@takken.io>
2023-10-10 00:50:24 +0200

    feat: map to ssi

--- src/components/pages/Tools/SsiDiveLogHelper/index.tsx
+++ src/components/pages/Tools/SsiDiveLogHelper/index.tsx
@@ -1,6 +1,5 @@
 import React, { createRef, useState } from 'react'
 import ToolPage from '@theme/ToolPage'
-import { Decoder, Stream } from '@garmin-fit/sdk'
 import { useGarminFiles } from '@site/src/domain/diving/garmin/GarminFiles'
 import { GarminDive } from '@site/src/domain/diving/garmin/GarminDive'
 import { GarminMessages } from '@site/src/domain/diving/garmin/GarminMessages'
@@ -23,48 +22,31 @@ const SsiDiveLogHelper = (): JSX.Element => {
   const [ssiDive, setSsiDive] = useState<Partial<SsiDive> | null>(null)
   const [diveQR, setDiveQR] = useState<string | null>(null)
   const [error, setError] = useState<string | null>(null)
-  const [firstName, setFirstName] = useState<string>()
-  const [lastName, setLastName] = useState<string>()
+  // const [firstName, setFirstName] = useState<string>()
+  // const [lastName, setLastName] = useState<string>()
   const garminFiles = useGarminFiles()
 
   const onUploadFile = async (): Promise<void> => {
-    // Select file
-
     const fileInput = fileInputRef.current
     if (!fileInput) return
 
     await garminFiles.add(fileInput.files)
 
-    for (const [fileName, bytes] of garminFiles) {
-      // Todo - render each file instead of one
-      console.log('reading', fileName)
-      const decoder = new Decoder(Stream.fromByteArray(bytes))
-
-      // Check integrity
-      if (!decoder.isFIT(bytes)) return setError('Unable to parse FIT file')
-      if (!decoder.checkIntegrity()) return setError('Integrity check failed')
-
-      // Todo - Scale messages / errors / display for multi-file
-
-      // Check for errors
-      const { messages, errors } = decoder.read({
-        // convertTypesToStrings: false,
-        includeUnknownData: false,
-        mergeHeartRates: true,
-      })
-      if (errors.length >= 1) return setError(errors.join(','))
-
-      // Update messages
-      setMessages(messages)
-      setError(null)
-
-      console.log(messages)
-      await parseDive(messages)
+    const errors = []
+    for (const dive of garminFiles) {
+      try {
+        setMessages(dive.messages)
+        await parseDive(dive)
+      } catch (error) {
+        errors.push(error)
+      }
     }
+
+    setError(errors.length >= 1 ? errors.join('\n') : null)
   }
 
-  const parseDive = async (messages: GarminMessages): Promise<void> => {
-    const dive = SsiDive.fromGarmin(new GarminDive(messages))
+  const parseDive = async (garminDive: GarminDive): Promise<void> => {
+    const dive = SsiDive.fromGarmin(garminDive)
 
     setSsiDive(dive)
     setDiveQR(SsiDive.toQR(dive))
@@ -81,30 +63,30 @@ const SsiDiveLogHelper = (): JSX.Element => {
           <li>Correct any details and save dive</li>
         </ul>
 
-        <div>
-          <label htmlFor="firstName" style={{ display: 'inline-block' }}>
-            <span style={{ display: 'block' }}>First name:</span>
-            <input
-              style={{ padding: 4, margin: '0 4px 4px 0', fontSize: '125%', width: 200 }}
-              type="text"
-              id="firstName"
-              name="firstName"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-          </label>
-          <label htmlFor="lastName" style={{ display: 'inline-block' }}>
-            <span style={{ display: 'block' }}>Last name:</span>
-            <input
-              style={{ padding: 4, margin: '0 4px 4px 0', fontSize: '125%', width: 200 }}
-              type="text"
-              id="lastName"
-              name="lastName"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            />
-          </label>
-        </div>
+        {/*<div>*/}
+        {/*  <label htmlFor="firstName" style={{ display: 'inline-block' }}>*/}
+        {/*    <span style={{ display: 'block' }}>First name:</span>*/}
+        {/*    <input*/}
+        {/*      style={{ padding: 4, margin: '0 4px 4px 0', fontSize: '125%', width: 200 }}*/}
+        {/*      type="text"*/}
+        {/*      id="firstName"*/}
+        {/*      name="firstName"*/}
+        {/*      value={firstName}*/}
+        {/*      onChange={(e) => setFirstName(e.target.value)}*/}
+        {/*    />*/}
+        {/*  </label>*/}
+        {/*  <label htmlFor="lastName" style={{ display: 'inline-block' }}>*/}
+        {/*    <span style={{ display: 'block' }}>Last name:</span>*/}
+        {/*    <input*/}
+        {/*      style={{ padding: 4, margin: '0 4px 4px 0', fontSize: '125%', width: 200 }}*/}
+        {/*      type="text"*/}
+        {/*      id="lastName"*/}
+        {/*      name="lastName"*/}
+        {/*      value={lastName}*/}
+        {/*      onChange={(e) => setLastName(e.target.value)}*/}
+        {/*    />*/}
+        {/*  </label>*/}
+        {/*</div>*/}
 
         <input
           type="file"
@@ -117,7 +99,12 @@ const SsiDiveLogHelper = (): JSX.Element => {
         <button
           type="button"
           onClick={() => fileInputRef.current?.click()}
-          style={{ padding: '8px 16px' }}
+          style={{
+            padding: '8px 16px',
+            backgroundColor: 'var(--ifm-color-primary)',
+            borderRadius: 5,
+            border: '1px solid var(--ifm-color-primary-light)',
+          }}
         >
           Select file
         </button>
@@ -131,6 +118,9 @@ const SsiDiveLogHelper = (): JSX.Element => {
             <h2>Key information</h2>
             <p>Scan the QR code in your SSI app</p>
             <p style={{ opacity: 0.5 }}>{diveQR}</p>
+            {/*<pre>*/}
+            {/*  <code style={{ opacity: 0.5 }}>{JSON.stringify(ssiDive, null, 2)}</code>*/}
+            {/*</pre>*/}
             <img
               alt="Dive QR code for scanning in SSI app"
               src={`https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=${diveQR}&cho=UTF-8`}
