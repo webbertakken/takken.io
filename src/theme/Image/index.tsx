1861994d1bda2382231ce91afd89143ecb87bf91
Webber Takken <webber@takken.io>
2023-11-25 18:12:56 +0100

    feat: add first poem (#127)
    * feat: add first poem
    
    * fix: formatting
    
    * fix: formatting
    
    * chore: sync slugs
    
    * fix: typo

---
+++ src/theme/Image/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './index.module.css'
+
+interface Props {
+  src: string
+  alt: string
+  year?: number
+}
+
+const Image = ({ src, alt, year, ...props }: Props): JSX.Element => {
+  return (
+    <div className={styles.wrapper}>
+      <img className={styles.image} {...props} src={src} alt={alt} />
+      {alt && (
+        <em className={styles.title}>
+          {alt}
+          {year && ` (${year})`}.
+        </em>
+      )}
+    </div>
+  )
+}
+
+export default Image
---
+++ src/theme/PoemAudio/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from './index.module.css'
+
+interface Props {
+  src: string
+  performer?: {
+    name: string
+  }
+  isSong?: boolean
+}
+
+const PoemAudio = (props: Props): JSX.Element => {
+  const { performer, isSong, ...audioProps } = props
+  return (
+    <div className={styles.main}>
+      <audio controls className={styles.audio} {...audioProps} />
+      {isSong ? (
+        <em>Lied. {performer ? `Gezongen door ${performer.name}.` : ''}</em>
+      ) : (
+        <em>Audioversie. {performer ? `Voorgelezen door ${performer.name}.` : ''}</em>
+      )}
+    </div>
+  )
+}
+
+export default PoemAudio
