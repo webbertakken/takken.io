fa1850b350471b4c6af4911f6eb530a3af597e43
Webber <webber@takken.io>
2023-11-25 17:38:46 +0100

    feat: add first poem

---
+++ src/theme/PoemText/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './index.module.css';
+
+interface Props {
+  children: string;
+}
+
+const PoemText = ({ children }: Props): JSX.Element => {
+  return (
+    <div>
+      <div className={styles.poem}>{children}</div>
+    </div>
+  );
+};
+
+export default PoemText;
