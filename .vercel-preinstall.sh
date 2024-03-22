1c6db67be9bc5b21ba1df91ec696b6e43cf3469e
Webber <webber@takken.io>
2024-03-22 20:07:05 +0100

    fix: vercel issue

---
+++ .vercel-preinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Using corepack to select the correct version of Node and Yarn."
+
+# Should already be installed in CI
+if ! command -v corepack >/dev/null 2>&1; then
+    echo "corepack is not installed. Installing..."
+    npm install -g corepack
+fi
+
+# Allow corepack to select the correct version of Node and Yarn
+corepack enable
+corepack install
