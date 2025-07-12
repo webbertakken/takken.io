6e39220895f1f9f320a32e5cbb3cce462fec250b
Webber <webber@takken.io>
2025-07-12 14:45:04 +0200

    feat: move to typescript image generator

--- tools/image-generator/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Takken.io Image Generator
-Production-grade image generation with consistent styling
-"""
-
-from .generator import TakkenImageGenerator
-
-__version__ = "1.0.0"
-__all__ = ['TakkenImageGenerator']
