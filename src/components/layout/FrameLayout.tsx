60a52916d0380606ba16ac4ab626384160963944
Webber <webber@takken.io>
2025-07-23 01:55:50 +0200

    chore: make it inclusive

--- src/components/layout/FrameLayout.tsx
+++ src/components/layout/FrameLayout.tsx
@@ -2,7 +2,7 @@ import styles from './FrameLayout.module.css'
 
 const FrameLayout = ({ children }) => (
   <>
-    <div className={styles.frame}></div>
+    <div className={styles.frame} />
     <div className={styles.body}>{children}</div>
   </>
 )
