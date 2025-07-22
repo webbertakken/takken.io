248a7a880ffaf376407de71d45350b9378a42796
Webber <webber@takken.io>
2025-07-23 01:19:23 +0200

    chore: simplify more

--- src/components/layout/FrameLayout.tsx
+++ src/components/layout/FrameLayout.tsx
@@ -2,14 +2,8 @@ import styles from './FrameLayout.module.css'
 
 const FrameLayout = ({ children }) => (
   <>
-    {/* Content */}
-    <div className={styles.frameBody}>{children}</div>
-
-    {/* Animations */}
-    <div className={styles.frameAnimated1}>
-      <div className={styles.frameAnimated2} />
-      <div className={styles.frameShadow} />
-    </div>
+    <div className={styles.frame}></div>
+    <div className={styles.body}>{children}</div>
   </>
 )
 
